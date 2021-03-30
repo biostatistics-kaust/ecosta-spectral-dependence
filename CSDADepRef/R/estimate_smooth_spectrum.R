5ae330a1a731136cd007225ae4dd2e1c9a668a1b
Marco <pinto.marco@live.com>
2021-03-31 01:05:40 +0200

    Fixing name

--- CSDADepRef/R/estimate_smooth_spectrum.R
+++
@@ -1,9 +0,0 @@
-#' Provides a smooth estimator of the spectrum of a signal
-#' @export
-estimate_smooth_spectrum <- function(x, fcut=0.05) {
-  Sx <- spec.pgram(x, taper=0, log="no", plot=FALSE)$spec
-  coeffs <- signal::butter(3, fcut, type="low")
-  pmax(0, signal::filtfilt(coeffs, Sx))
-}
-
-
